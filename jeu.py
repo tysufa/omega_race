f9ad4778b9fbf79716aa6a60d7f103261c4a292a
Philemon Penot <grandours91@gmail.com>
2023-04-18 15:19:20 +0200

    amélioration classe Game

--- jeu.py
+++ jeu.py
@@ -1,4 +1,5 @@
 import pygame
+from player import Player
 
 pygame.init()
 
@@ -11,11 +12,27 @@ class Game:
         self.center_square = pygame.rect.Rect((0, 0, size[0]//3, size[1]//3)) # on fait le carré principale en fonction de la taille de la fenetre
         self.center_square.center = (size[0]//2, size[1]//2) # on place le carré au centre de l'écran
 
+        self.score = 100
+        self.high_score = 0
+        self.ariel = pygame.font.SysFont("Comic Sans MS", 30) # on créer la police de caractère ariel
+
+        self.score_text1_surface = self.ariel.render("score", False, "white") # le texte à afficher
+        self.score_text1_rect = self.score_text1_surface.get_rect() # le rectangle pour avoir la position du text
+        self.score_text1_rect.topright = self.center_square.topright # on place le text dans l'angle en haut à droite du rectangle
+        self.score_text1_rect.x -= 10; self.score_text1_rect.y += 10 # légère correction sur la position
+
+        # on refait la même chose pour l'affichage de la valeur du score
+        self.score_text2_surface = self.ariel.render(str(self.score), False, "white")
+        self.score_text2_rect = self.score_text2_surface.get_rect()
+        self.score_text2_rect.topright = self.score_text1_rect.bottomright
+
         self.clock = pygame.time.Clock()
 
     def draw(self):
         self.window.fill("black")
         pygame.draw.rect(self.window, "white", self.center_square, 5)
+        self.window.blit(self.score_text1_surface, self.score_text1_rect)
+        self.window.blit(self.score_text2_surface, self.score_text2_rect)
 
     def run(self):
         continuer = True
---
+++ player.py
@@ -0,0 +1,7 @@
+import pygame
+
+pygame.init()
+
+class Player:
+    def __init__(self):
+        self.
