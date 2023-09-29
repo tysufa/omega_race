b4281505c76f62d5dabd11b5ea0e90523a42bce8
CampesatoFan <leonard@boeuf.org>
2023-09-29 21:59:56 +0200

    un pull pour emener le jeu ce weekend

--- constantes.py
+++ constantes.py
@@ -19,9 +19,9 @@ ROTATION_SPEED = 5
 PLAYER_IMAGE = "image/Kla'ed/Base/Kla'ed - Frigate - Base.png"
 PLAYER_INITIAL_POSITION = (130, 160)
 RESPAWN_TIME = 50 * 9 # durée de l'animation de mort x nombre de frame sur l'animation de la mort
-LIFE_NB = 0
+LIFE_NB = 2
 
-FIRE_RATE = 550 # temps en millisecodes
+FIRE_RATE = 550 # temps en millisecodes was 550
 BULLET_SPEED = MAX_PLAYER_SPEED * 1.7 * PLAYER_SPEED
 BULLET_SPRITESHEET = "image/Kla'ed/Projectiles/Kla'ed - Big Bullet.png"
 
