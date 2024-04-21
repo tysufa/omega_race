bd1ee81cf8107652033459569fccd8ff6779d107
tysufa <philemon.penot@gmail.com>
2024-04-21 23:09:07 +0200

    simple renommages des ressources

--- text.py
+++ text.py
@@ -4,7 +4,7 @@ pygame.init()
 
 
 class Text(pygame.sprite.Sprite):
-    def __init__(self, text, size, x, y, color, font="ressources/Abaddon Light.ttf"):
+    def __init__(self, text, size, x, y, color, font="src/Abaddon Light.ttf"):
         super().__init__()
         self.font = font
         self.text = text
@@ -12,7 +12,7 @@ class Text(pygame.sprite.Sprite):
         self.y = y
         self.color = color
         self.size = size
-         
+
         self.font = pygame.font.Font(font, self.size)
         self.image = self.font.render(text, True, self.color)
         self.rect = self.image.get_rect(topright=(self.x, self.y))
@@ -20,4 +20,7 @@ class Text(pygame.sprite.Sprite):
     def change_text(self, text, change_pos=True):
         self.image = self.font.render(text, True, self.color)
         if change_pos:
-            self.rect = self.image.get_rect(topright=(self.x, self.y)) # on veut déplacer le text du score mais pas celui du menu par exemple
+            self.rect = self.image.get_rect(
+                topright=(self.x, self.y)
+            )  # on veut déplacer le text du score mais pas celui du menu par exemple
+
